b4d5d5200d7567f1bd0cdf3fed5784f4e032141c
512jay <512jay@gmail.com>
2018-10-31 11:07:44 -0400

    Updates

---
+++ precache-manifest.391264db10b255727900a35ad0f44304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/myapp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "d4d3bad05211dbbf453f",
+    "url": "/myapp/static/js/main.d4d3bad0.chunk.js"
+  },
+  {
+    "revision": "a9bc790168e3f36f99d1",
+    "url": "/myapp/static/js/1.a9bc7901.chunk.js"
+  },
+  {
+    "revision": "d4d3bad05211dbbf453f",
+    "url": "/myapp/static/css/main.9f2108d9.chunk.css"
+  },
+  {
+    "revision": "a9bc790168e3f36f99d1",
+    "url": "/myapp/static/css/1.3e269d68.chunk.css"
+  },
+  {
+    "revision": "28bd9d896c4d069ff2226d9e3e1658ee",
+    "url": "/myapp/index.html"
+  }
+];
