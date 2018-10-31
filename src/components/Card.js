4cf443f5f8f6db75d578f9cf69480c1eba4e3151
512jay <512jay@gmail.com>
2018-10-31 10:50:23 -0400

    added redux

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({id, name, username, email}) => {
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
+            <img alt="robots" src={`https://robohash.org/${id}?200x200`} height="200" width="200" />
+            <div>
+                <h2>{name}</h2>
+                <p>{username}</p>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
